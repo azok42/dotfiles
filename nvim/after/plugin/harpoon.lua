6d8467d7f49239fc216161ed38d6d4b12bc810ef
Azok42 <anton.hackner@sz-ybbs.ac.at>
2026-06-06 16:10:11 +0200

    refactor: move remaps to lua/remaps.lua

--- nvim/after/plugin/harpoon.lua
+++ nvim/after/plugin/harpoon.lua
@@ -3,16 +3,3 @@ local harpoon = require("harpoon")
 -- REQUIRED
 harpoon:setup()
 -- REQUIRED
-
-vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
-vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-
-vim.keymap.set("n", "<A-1>", function() harpoon:list():select(1) end)
-vim.keymap.set("n", "<A-2>", function() harpoon:list():select(2) end)
-vim.keymap.set("n", "<A-3>", function() harpoon:list():select(3) end)
-vim.keymap.set("n", "<A-4>", function() harpoon:list():select(4) end)
-vim.keymap.set("n", "<A-5>", function() harpoon:list():select(5) end)
-
--- Toggle previous & next buffers stored within Harpoon list
-vim.keymap.set("n", "<C-S-H>", function() harpoon:list():prev() end)
-vim.keymap.set("n", "<C-S-L>", function() harpoon:list():next() end)
--- nvim/lua/remaps.lua
+++ nvim/lua/remaps.lua
@@ -10,6 +10,35 @@ vim.keymap.set("n", "<leader>gsh", function() require('gitsigns').stage_hunk() e
 -- undotree keymaps
 vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
 
+-- harpoon
+local harpoon = require("harpoon")
+vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+vim.keymap.set("n", "<A-1>", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<A-2>", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<A-3>", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<A-4>", function() harpoon:list():select(4) end)
+vim.keymap.set("n", "<A-5>", function() harpoon:list():select(5) end)
+
+vim.keymap.set("n", "<C-S-H>", function() harpoon:list():prev() end)
+vim.keymap.set("n", "<C-S-L>", function() harpoon:list():next() end)
+
+-- neogen
+local neogen = require("neogen")
+vim.keymap.set("n", "<leader>df", neogen.generate, { desc = "Generate Documentation" })
+
+-- telescope
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Telescope git files' })
+vim.keymap.set('n', '<leader>ps', function() 
+	builtin.grep_string({ search  = vim.fn.input("Grep > ") });
+end, { desc = 'Telescope find files' })
+
+-- undotree
+vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
+
 -- lsp
 vim.keymap.set("n", "gd", vim.lsp.buf.definition)
 vim.keymap.set("n", "K", vim.lsp.buf.hover)
@@ -17,3 +46,13 @@ vim.keymap.set('n', 'E', vim.diagnostic.open_float)
 vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action)
 vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references)
 vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename)
+
+-- compiler
+vim.api.nvim_set_keymap('n', '<leader>C', "<cmd>CompilerOpen<cr>", { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap('n', '<leader>c',
+     "<cmd>CompilerStop<cr>" -- (Optional, to dispose all tasks before redo)
+  .. "<cmd>CompilerRedo<cr>",
+ { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap('n', '<leader>Ct', "<cmd>CompilerToggleResults<cr>", { noremap = true, silent = true })
