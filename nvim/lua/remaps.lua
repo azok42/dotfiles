d3b5b6944e730e89f899bb9f2dd5ac4a3e2c5c1a
Azok42 <anton1.hackner@gmail.com>
2026-07-25 21:34:31 +0200

    refactor(nvim): make harpoon quick select keymaps cleaner

--- nvim/lua/remaps.lua
+++ nvim/lua/remaps.lua
@@ -23,16 +23,10 @@ local harpoon = require("harpoon")
 vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
 vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-vim.keymap.set("n", "<A-1>", function() harpoon:list():select(1) end)
-vim.keymap.set("n", "<A-2>", function() harpoon:list():select(2) end)
-vim.keymap.set("n", "<A-3>", function() harpoon:list():select(3) end)
-vim.keymap.set("n", "<A-4>", function() harpoon:list():select(4) end)
-vim.keymap.set("n", "<A-5>", function() harpoon:list():select(5) end)
-vim.keymap.set("n", "<A-6>", function() harpoon:list():select(6) end)
-vim.keymap.set("n", "<A-7>", function() harpoon:list():select(7) end)
-vim.keymap.set("n", "<A-8>", function() harpoon:list():select(8) end)
-vim.keymap.set("n", "<A-9>", function() harpoon:list():select(9) end)
-vim.keymap.set("n", "<A-0>", function() harpoon:list():select(0) end)
+-- harpoon quick selects
+for i = 0, 9 do
+   vim.keymap.set("n", "<A-" .. i .. ">", function() harpoon:list():select(i) end)
+end
 
 vim.keymap.set("n", "<C-S-H>", function() harpoon:list():prev() end)
 vim.keymap.set("n", "<C-S-L>", function() harpoon:list():next() end)
