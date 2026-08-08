06ed64ac63b632e4aae89c39c48591f171ef69ac
Azok42 <anton1.hackner@gmail.com>
2026-08-09 00:10:31 +0200

    feat(nvim): add wakatime today status to lualine

--- nvim/after/plugin/lualine.lua
+++ nvim/after/plugin/lualine.lua
@@ -1,3 +1,33 @@
+local wakatime_cache = "Loading..."
+
+local function fetch_wakatime()
+   vim.fn.jobstart({"wakatime-cli", "--today"}, {
+      stdout_buffered = true,
+      on_stdout = function(_, data)
+         if data and data[1] and data[1] ~= "" then
+            wakatime_cache = data[1]
+         else
+            wakatime_cache = "Error (fetch)"
+         end
+      end
+   })
+end
+
+fetch_wakatime()
+
+local timer = (vim.uv or vim.loop).new_timer()
+
+if timer == nil then
+   wakatime_cache = "Error (timer)"
+   return
+end
+
+timer:start(120000, 120000, vim.schedule_wrap(fetch_wakatime))
+
+local function get_wakatime()
+   return wakatime_cache
+end
+
 require('lualine').setup {
   options = {
     icons_enabled = true,
@@ -34,7 +64,7 @@ require('lualine').setup {
   sections = {
     lualine_a = {'mode'},
     lualine_b = {'branch', 'diff', 'diagnostics'},
-    lualine_c = {'filename'},
+    lualine_c = {get_wakatime, 'filename'},
     lualine_x = {'encoding', 'fileformat', 'filetype'},
     lualine_y = {'progress'},
     lualine_z = {'location'}
